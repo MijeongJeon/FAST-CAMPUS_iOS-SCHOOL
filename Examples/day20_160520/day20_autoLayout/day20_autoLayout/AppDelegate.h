048a82f5227e3f5da7c53c0f763c1af2b0bccbe2
Mijeong Jeon <ninevincentg@gmail.com>
2016-05-22 19:22:33 +0900

    add Example day 20

---
+++ Examples/day20_160520/day20_autoLayout/day20_autoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day20_autoLayout
+//
+//  Created by Mijeong Jeon on 5/20/16.
+//  Copyright © 2016 Mijeong Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/day20_160520/day20_autoLayout/day20_autoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day20_autoLayout
+//
+//  Created by Mijeong Jeon on 5/20/16.
+//  Copyright © 2016 Mijeong Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
