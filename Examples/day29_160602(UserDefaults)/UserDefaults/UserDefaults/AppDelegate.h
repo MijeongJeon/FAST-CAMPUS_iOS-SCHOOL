e340dd68d29b06084ee4dcaa17d46faf3b35865c
Mijeong Jeon <ninevincentg@gmail.com>
2016-06-03 02:32:22 +0900

    2016-06-03 UPDATE

---
+++ Examples/day29_160602(UserDefaults)/UserDefaults/UserDefaults/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserDefaults
+//
+//  Created by Mijeong Jeon on 6/2/16.
+//  Copyright © 2016 Mijeong Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework/day20_LoginPage/LoginPage/LoginPage/MainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainPageViewController.h
+//  LoginPage
+//
+//  Created by Mijeong Jeon on 6/2/16.
+//  Copyright © 2016 Mijeong Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainPageViewController : UIViewController
+
+@end
