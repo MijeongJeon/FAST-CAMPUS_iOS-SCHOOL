a6e6ac2fb5d7208ba268a9e74d40e3cc63110abc
Mijeong Jeon <ninevincentg@gmail.com>
2016-06-07 23:31:12 +0900

    2016-06-07 Modify Method
    Move isCheck ID and Password to DataCenter
    Add Alert to notify unfilled field
    Add Alert fo notyfy uncorrect user information

--- Homework/day20_LoginPage/LoginPage/LoginPage/ViewController.m
+++ Homework/day20_LoginPage/LoginPage/LoginPage/ViewController.m
@@ -29,27 +29,34 @@
     _idField.tag = 1;
     _passwordField.delegate = self;
     _passwordField.tag = 2;
+    
+    if (!([[DataCenter userDefaults] objectForKey:@"autoId"] == nil)) {
+    _idField.text = [[DataCenter userDefaults] objectForKey:@"autoId"];
+    }
     [self displayMemberInfo];
     [self.navigationController setNavigationBarHidden:YES];
-
 }
-
 
 // 메인화면으로 이동하는 세그
 - (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    
     if ([identifier isEqualToString:@"LoginToMain"]) {
-        if ([self isCheckLoginwithID:_idField.text userPW:_passwordField.text]) {
-            [[DataCenter setUserDefaults] setObject:_idField.text forKey:@"autoId"];
-            [[DataCenter setUserDefaults] setBool:NO forKey:@"autoBool"];
-            return YES;
+        if (_idField.text.length > 0 && _passwordField.text.length > 0) {
+            if ([[DataCenter sharedInstance] isCheckLoginwithID:_idField.text userPW:_passwordField.text]) {
+                [[DataCenter userDefaults] setObject:_idField.text forKey:@"autoId"];
+                [[DataCenter userDefaults] setBool:YES forKey:@"autoBool"];
+                return YES;
+            } else {
+                [self showAlert:@"Login Fail" andMessage:@"Invalid User Information" andidField:_idField andVC:self];
+                return NO;
+            }
         } else {
-            return NO;
+            [self showAlert:@"Insert Field" andMessage:@"Inserd ID and PW" andidField:_idField andVC:self];
         }
     } else {
-        return YES;
     }
+        return YES;
 }
-
 
 // ID입력후 엔터 누르면 다음칸으로 이동, PW입력후 엔터 누르면 버튼 클릭
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -58,36 +65,43 @@
         return YES;
     }
     if (textField.tag == 2 && _passwordField.text.length > 0) {
-        if ([self shouldPerformSegueWithIdentifier:@"LoginToMain" sender:_buttonLogin]) {
-            [self performSegueWithIdentifier:@"LoinToMain" sender:self];
+        if ([self shouldPerformSegueWithIdentifier:@"LoginToMain" sender:self]) {
+            [self performSegueWithIdentifier:@"LoginToMain" sender:self];
         }
-        
     }
     return YES;
 }
 
+// 알림 표시
 
-// ID와 Password가 가입되어있는 정보인지 확인하는 메소드
-- (BOOL)isCheckLoginwithID:(NSString *)userID userPW:(NSString *)userPW {
-    
-    NSArray *infoArray = [NSArray arrayWithContentsOfFile:[[DataCenter sharedInstance] findUserInfoPath]];
-    for (NSDictionary *infoDic in infoArray) {
-        NSString *insertedID = [infoDic objectForKey:@"id"];
-        NSString *insertedPW = [infoDic objectForKey:@"password"];
-        if ([insertedID isEqualToString:userID] && [insertedPW isEqualToString:userPW]) {
-            return YES;
+- (void)showAlert:(NSString *)failTitle andMessage:(NSString *)failMessage andidField:(UITextField *)idField  andVC:(UIViewController *)VC{
+    id block = ^(UIAlertAction * _Nonnull action) {
+        
+        if ([action.title isEqualToString:@"Try Agian"]) {
+            [idField becomeFirstResponder];
         }
-    }
-    return NO;
+    };
+    
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:failTitle
+                                          message:failMessage
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *tryAgainction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:block];
+    [alertController addAction:tryAgainction];
+    [VC presentViewController:alertController animated:YES completion:^(void) {[idField becomeFirstResponder];}];
 }
+
 
 //  가입 id 화면에 표시
 - (void)displayMemberInfo {
     NSArray *infoArray = [NSArray arrayWithContentsOfFile:[[DataCenter sharedInstance] findUserInfoPath]];
     for (NSDictionary *infoDic in infoArray) {
-        NSLog(@"%@",[infoDic objectForKey:@"id"]);
+        NSLog(@"registered id : %@",[infoDic objectForKey:@"id"]);
     }
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
