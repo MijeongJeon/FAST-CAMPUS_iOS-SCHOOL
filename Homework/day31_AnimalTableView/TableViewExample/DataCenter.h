0344b9f38993ef15633a1de83b73a0b1d8184986
Mijeong Jeon <ninevincentg@gmail.com>
2016-06-07 23:39:29 +0900

    2016-06-07 AnimalTableView

---
+++ Homework/day31_AnimalTableView/TableViewExample/DataCenter.h
@@ -0,0 +1,20 @@
+//
+//  DataCenter.h
+//  TableViewExample
+//
+//  Created by youngmin joo on 2016. 6. 6..
+//  Copyright © 2016년 WingsCompany. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
++ (instancetype)defaultData;
+
+- (NSDictionary *)allAnimals;
+- (NSArray *)sectionTitles;
+- (NSInteger)sectionCount;
+- (NSString *)imageNameWithAnimal:(NSString *)animal;
+
+@end
---
+++ Homework/day31_AnimalTableView/TableViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewExample
+//
+//  Created by youngmin joo on 2016. 6. 6..
+//  Copyright © 2016년 WingsCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
